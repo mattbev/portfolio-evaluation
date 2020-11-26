79801b11f5095c4ac5d36b3cdbda61656a51b2ba
Matt Beveridge <mattbev@mit.edu>
2020-11-26 05:10:00 -0500

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # portfolio-evaluation
+
+## Minimum Volatility Portfolio (MVP)
+see `autoportfolio.optimize.minimum_volatility `
+
+## Minimum Tracking Error Portfolio (MTEP)
+see `autoportfolio.optimize.minimum_tracking_error`
+
+## Covariance Matrix Calculation and MVP on Real Date
+see `simulation.ipynb`
--- autoportfolio/optimize.py
+++ autoportfolio/optimize.py
@@ -2,6 +2,7 @@ import cvxpy as cp
 import numpy as np
 from scipy.optimize import minimize
 
+
 def minimum_volatility(cov_mat, w_lower=0.0, w_upper=1.0, convex=False):
     """
     determine the portfolio allocations w that minimize volatility
@@ -49,19 +50,30 @@ def minimum_volatility(cov_mat, w_lower=0.0, w_upper=1.0, convex=False):
         return res.x
 
 
-def minimum_tracking_error(cov_mat, w_lower=0.0, w_upper=1.0, convex=True):
+def minimum_tracking_error(cov_mat, w_lower=0.0, w_upper=1.0, convex=False):
+    """
+    determine the portfolio allocations w that minimize tracking error
+
+    Args:
+        cov_mat (numpy array): the variance covariance matrix
+        w_lower (float, optional): lower bound for allocation proportion. Defaults to 0.0.
+        w_upper (float, optional): upper bound for allocation proportion. Defaults to 1.0.
+        convex (bool, optional): whether the problem is convex or not; convexity allows a 
+                                 speedup. Defaults to False.
+
+    Returns:
+        (numpy array): the values that produce the minimum tracking error portfolio
+    """        
     m, n = cov_mat.shape
     assert m == n
     assert w_lower <= w_upper
 
     sigma_bm = cov_mat[m-1][m-1]
     rho_p_bm = cov_mat[:,m-1]
-    print(sigma_bm)
-    print(rho_p_bm)
 
     if convex:
         w = cp.Variable(m)
-        objective = cp.Minimize(cp.sqrt(cp.quad_form(w, cov_mat)))
+        objective = cp.Minimize(cp.sqrt(cp.quad_form(w, cov_mat) * np.sum(1 - 2*sigma_bm*rho_p_bm) + sigma_bm**2))
         constraints = [w >= w_lower, w <= w_upper, sum(w) == 1]
         problem = cp.Problem(objective, constraints)
 
@@ -86,3 +98,4 @@ def minimum_tracking_error(cov_mat, w_lower=0.0, w_upper=1.0, convex=True):
             constraints=constraints
         )
         return res.x
+
